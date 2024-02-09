592ba32ce0703ef6b87e2c83c8b2b3644e8b7cba
BinayND <39079489+BinayND@users.noreply.github.com>
2024-02-09 12:24:07 +0530

    validator for skip

--- Express.js
+++ Express.js
@@ -9,12 +9,12 @@
 
 
 ### Validation
+  
+-While using express validator package If you'd like to skip validations when the value is null, undefined, false, zero or an empty string, you can do this:
+```
+check('name').optional({ checkFalsy: true }).trim().isLength({ min: 3, max: 30 }),
 
 ```
-
-
-```
-
 
 ### Errorhandling
 
